4203ededab3230b2c5db0577c66c62592c4ead1c
Ahmed_Reda_Morsi <44190899+Ahmed-Reda-Morsi@users.noreply.github.com>
2023-10-01 16:10:43 +0300

    update  isValidAcount

--- Server/server.c
+++ Server/server.c
@@ -1,73 +1,91 @@
 #include"server.h"
 
+uint32_t totalNumOfTransactions = 0;
+
 EN_transState_t recieveTransactionData(ST_transaction_t *transData)
 {
 
 }
 
-EN_serverError_t isValidAccount(ST_cardData_t *cardData, ST_accountsDB_t *accountRefrence)\
+EN_serverError_t isValidAccount(ST_cardData_t *cardData, ST_accountsDB_t *accountRefrence)
 {
-
 }
-//_______________| implementation isBlockAccount |____________________________
-/* This function takes a reference to the account into the database and verifies
-    if it is blocked or not. */
 EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountRefrence)
 {
-    uint8_t errorStatus=SERVER_OK;                       // return variable for error statu 
-    // check if the account status is running or not
-    if (accountRefrence->state!=RUNNING)
-    {
-        errorStatus=BLOCKED_ACCOUNT;
-    }
 
-    return errorStatus;
 }
-
-//_______________| implementation isBlockedAccountTest |______________________
-void isBlockedAccountTest(void)
-{
-    ST_accountsDB_t accountDBTest[3]={{0,RUNNING},{0,BLOCKED},{0}};
-    uint8_t expectedResult[3]={SERVER_OK,BLOCKED_ACCOUNT,SERVER_OK};
-    //   test card module.
-    printf("//__________________|isBlockedAccount function testCases. |________________ \n");
-    printf("Tester Name: Ahmed Reda\nFunction Name: isBlockedAccount\n");
-    printf("input meaning: 0-> running | 1-> Blocked\n");
-    
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        uint8_t callingResult=isBlockedAccount(&accountDBTest[i]);                                         // get result of calling getTransactionDate                            
-        if (expectedResult[i]==SERVER_OK && callingResult==SERVER_OK)
-        {
-            printf("Test Case %d:\nIntput: %d\nExpected Result:%s\nActual Result: %s\n",i,accountDBTest[i].state,"SERVER_OK","SERVER_OK");
-        }
-        else if (expectedResult[i]==BLOCKED_ACCOUNT && callingResult==SERVER_OK)
-        {
-            printf("Test Case %d:\nIntput: %d\nExpected Result:%s\nActual Result: %s\n",i,accountDBTest[i].state,"BLOCKED_ACCOUNT","SERVER_OK");
-        }
-        else if (expectedResult[i]==BLOCKED_ACCOUNT && callingResult==BLOCKED_ACCOUNT)
-        {
-            printf("Test Case %d:\nIntput: %d\nExpected Result:%s\nActual Result: %s\n",i,accountDBTest[i].state,"BLOCKED_ACCOUNT","BLOCKED_ACCOUNT");
-        }
-        else if (expectedResult[i]==SERVER_OK && callingResult==BLOCKED_ACCOUNT)
-        {
-            printf("Test Case %d:\nIntput: %d\nExpected Result:%s\nActual Result: %s\n",i,accountDBTest[i].state,"SERVER_OK","BLOCKED_ACCOUNT");
-        }
-        else
-        {/*DO NOTHING.*/}
-        
-    }
-}
-
 EN_serverError_t isAmountAvailable(ST_terminalData_t *termData, ST_accountsDB_t *accountRefrence)
 {
 
 }
 EN_serverError_t saveTransaction(ST_transaction_t *transData)
 {
+    FILE *fptr; // File pointer for transactions database
+    fptr = fopen("Transactions DB.txt", "a"); // Open the file in append mode
+
+    if (!fptr)
+    {
+        // If the file cannot be opened, display an error message
+        printf("Transactions DB is not available.\n");
+        return INTERNAL_SERVER_ERROR; // Return the error code for internal server error
+    }
+
+    transData->transactionSequenceNumber = totalNumOfTransactions + 1; // Assign the transaction sequence number
+    totalNumOfTransactions++; // Increment the total number of transactions
+
+    // Write transaction details to the file
+    fprintf(fptr, "Transaction Number:%d", transData->transactionSequenceNumber);
+    fputs(",", fptr);
+    fprintf(fptr, "Transaction Date:%s", transData->terminalData.transactionDate);
+    fputs(",", fptr);
+    fprintf(fptr, "Card Holder Name:%s", transData->cardHolderData.cardHolderName);
+    fputs(",", fptr);
+    fprintf(fptr, "PAN:%s", transData->cardHolderData.primaryAccountNumber);
+    fputs(",", fptr);
+    fprintf(fptr, "EXP Date:%s", transData->cardHolderData.cardExpirationDate);
+    fputs(",", fptr);
+    fprintf(fptr, "Amount:%.2f", transData->terminalData.transAmount);
+    fputs(",", fptr);
+    fprintf(fptr, "Max Amount:%.2f", transData->terminalData.maxTransAmount);
+    fputs(",", fptr);
+
+    // Write the transaction state to the file
+    if (transData->transState == APPROVED)
+        fprintf(fptr,"Transaction State: Approved\n");
+    else if (transData->transState == DECLINED_INSUFFECIENT_FUND)
+        fprintf(fptr,"Transaction State: Declined Insufficient Fund\n");
+    else if (transData->transState == DECLINED_STOLEN_CARD)
+        fprintf(fptr,"Transaction State: Declined Stolen Card\n");
+    else if (transData->transState == FRAUD_CARD)
+        fprintf(fptr,"Transaction State: Fraud Card\n");
+    else
+        fprintf(fptr,"Transaction State: Internal Server Error\n");
+
+    fprintf(fptr,"==================================================\n");
+
+    fclose(fptr); // Close the file
+    return SERVER_OK; // Return the success code
+}
+
+void saveTransactionTest(void)
+{
+    ST_transaction_t transData;
+	printf("Tester Name: Ahmed Hazem Kamal\n");
+    printf("Function Name: saveTransaction\n");
+    printf("---------------------------\n");
+
+    // Initialize transaction data with sample values
+	strcpy(transData.cardHolderData.cardHolderName, "AhmedHazemKamalEldinAb");
+	strcpy(transData.cardHolderData.primaryAccountNumber, "8989374615436851");
+	strcpy(transData.cardHolderData.cardExpirationDate, "09/29");
+	transData.terminalData.transAmount = 1000.0;
+	transData.terminalData.maxTransAmount = 1200.0;
+    strcpy(transData.terminalData.transactionDate, "30/09/2023");
+    // Save the transaction to the database
+	saveTransaction(&transData);
 
 }
 void listSavedTransactions(void)
 {
-    
+
 }
